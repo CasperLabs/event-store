2ffcecb8136f428c89d769c120db4d35928a4f47
George <george@casperlabs.io>
2020-11-01 14:42:25 +0000

    WIP Adds handling of malformed and duplicate event

--- src/storage.js
+++ src/storage.js
@@ -1,48 +1,117 @@
 class Storage {
     constructor(models) {
         this.models = models;
-        this.malformedBlocks = [];
-        this.duplicatedBlocks = [];
     }
-    
-    /**
-     * Error handling needs discussed and improved 
-     */
 
+    arraysMatch(a, b) {
+        return Array.isArray(a) &&
+            Array.isArray(b) &&
+            a.length === b.length &&
+            a.every((val, index) => val === b[index]);
+    }
+
+
+    /**
+     * This logic needs work 
+     */
+    async deploysExist(event) {
+        let eventDeploys = event.proto_block.deploys;
+        
+        if (eventDeploys.length !== 0) {
+        // event has deploys
+            for (let i=0, numDeploys = eventDeploys.length; i <= numDeploys; i++) {
+                // Look for a deploy in the db with the hash
+                let check = await this.findDeployByHash(eventDeploys[i]);
+                if (!check) {
+                    // event has deploy not present in db return it for logging
+                    return eventDeploys[i];
+                }
+            }
+            // all deploys in event are present in db
+            return true;
+        } else {
+        // event has no deploys
+            return null;
+        }
+       
+    }
+
+    async checkFinalizedBlockEvent(event) {
+        let existingBlock = await this.models.Block.findOne({where: {blockHeight: event.height}});
+        if ( existingBlock !== null) {
+            // There is already a block at the current height
+            console.warn("\n\tWARN: blockHeight not unique: \n\tThere is already a block at height: " + event.height + "\n");
+            
+            // Setup data
+            let existingBlockData = existingBlock.dataValues;
+            
+
+            // Check data for matches
+            let timeMatches, eraMatches, proposerMatches, deploysMatch;
+            timeMatches = existingBlockData.timestamp.toISOString() === event.timestamp;
+            eraMatches = existingBlockData.eraId === event.era_id;
+            proposerMatches = existingBlockData.proposer === event.proposer;
+
+            deploysMatch = await this.deploysExist(event);
+            
+            // Log findings
+            console.log("\nComparing events...");
+
+            console.log("\ntimestamp: \t" + (timeMatches ? "Matches" : ("Does not match:\t" + 
+            existingBlockData.timestamp.toISOString() + " !== " + event.timestamp)));
+
+            console.log("eraId: \t\t" + (eraMatches ? "Matches" : ("Does not match:\t" + 
+            existingBlockData.eraId + " !== " + event.era_id)));
+
+            console.log("proposer: \t" + (proposerMatches ? "Matches" : ("Does not match:\t" + 
+            existingBlockData.proposer + " !== " + event.proposer)));
+
+            // Handle Deploys
+            process.stdout.write("Deploys: \t");
+            if (deploysMatch === true) {
+                console.log("Matches");
+            } else if ( deploysMatch === null) {
+                console.log("No deploys in event to check");
+            } else {
+                console.log("No exisiting deploy with hash: " + deploysMatch)
+            }
+            
+            console.log("\n\t--End of Comparison--\n\n");
+
+            if (timeMatches &&
+                eraMatches &&
+                proposerMatches && 
+                deploysMatch) {
+                    // Incoming event is an exact duplicate of an existing entry
+                    return true;
+            } else {
+                // Incoming event shares height but is not a exact duplicate
+                // NEED TO DECIDE WHAT TO DO IN THIS SITUATION
+                return true;
+            }
+        }
+    }
 
     async onFinalizedBlock(event) {
-        await this.models.Block.create({
-            blockHeight: event.height,
-            blockHash: null,
-            timestamp: event.timestamp,
-            eraId: event.era_id,
-            proposer: event.proposer,
-            state: 'finalized',
-            Deploys: event.proto_block.deploys.map(deployHash => {
-                return {
-                    deployHash: deployHash,
-                    state: 'finalized'
-                }
-            })
-        }, {
-            include: [ this.models.Block.Deploys ]
-        }).catch(err => {
-            if(err instanceof this.models.Sequelize.UniqueConstraintError){
-                console.error(
-                "\nError: Duplicated blockHeight in event" + 
-                "\nDuplicate block with height " + event.height + " will not be saved\n");
-                this.duplicatedBlocks.push(event.height);
-            }
-            else if (err instanceof this.models.Sequelize.ValidationError) {
-                console.error(
-                "\nValidation Issue - this is probably due to an incorrectly formatted field in the event" + 
-                "\nBlock with height " + event.height + " will not be saved\n");
-                this.malformedBlocks.push(event.height);
-            }
-            else{
-                throw err
-            }
-        });
+        if ( !(await this.checkFinalizedBlockEvent(event)) ) {
+            // There is no block at the current height
+            await this.models.Block.create({
+                blockHeight: event.height,
+                blockHash: null,
+                timestamp: event.timestamp,
+                eraId: event.era_id,
+                proposer: event.proposer,
+                state: 'finalized',
+                Deploys: event.proto_block.deploys.map(deployHash => {
+                    return {
+                        deployHash: deployHash,
+                        state: 'finalized'
+                    }
+                })
+            }, {
+                include: [ this.models.Block.Deploys ]
+            });
+        }       
     }
 
     async onDeployProcessed(event) {
@@ -55,14 +124,16 @@ class Storage {
     }
 
     async onBlockAdded(event) {
-        if (!this.malformedBlocks.includes(event.block_header.height)) {
-            console.log(this.malformedBlocks);
-            let block = await this.findBlockByHeight(event.block_header.height);
+        let block = await this.findBlockByHeight(event.block_header.height);
+        if (block === null) {
+            console.warn("\n\tWARN: block missing at height: " + event.block_header.height);
+            console.warn("\tThis might be due to a problem in the corresponding blockFinalized event\n");
+        } else {
             block.state = 'added';
             block.parentHash = event.block_header.parent_hash;
             block.blockHash = event.block_hash;
             await block.save();
-        }
+        }    
     }
 
     async findBlockByHeight(height) {
