28c466c2b1998e41510826389f0f9ad7d182614e
zie1ony <maciej@casperlabs.io>
2020-11-03 17:43:40 +0100

    blocks ws in progress

--- src/wsServer.js
+++ src/wsServer.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var expressWs = require('express-ws');
 
-let wsServer = (models) => {
+let wsServer = (pubsub) => {
     var app = express();
     var wsApp = expressWs(app);
 
-    app.ws('/', function(ws, req) {
-        ws.on('message', function(msg) {
-            ws.send("hej");
+    app.ws('/blocks', function(ws, req) {
+        pubsub.on_block( (block) => {
+            console.log("on block");
+            ws.send(block);
         });
         console.log('socket open');
     });
-
+    
     return wsApp;
 }
 
--- test/testWsServer.js
+++ test/testWsServer.js
@@ -1,37 +1,41 @@
 const chai = require('chai');
 const WebSocket = require('ws');
+const redis = require("redis-mock");
 const assert = chai.assert;
+
 const models = require('../src/models/index');
 const Storage = require('../src/storage');
 const wsServer = require('../src/wsServer');
+const PubSub = require('../src/pubsub');
+
 const data = require('./mockData');
 
 var wsApp = null;
 var server = null;
 var client = null;
+var pubsub = null;
 
 describe('WebSocket Server', async () => {
     beforeEach(async () => {
-        wsApp = wsServer(models);
+        pubsub = new PubSub(redis.createClient());
+        wsApp = wsServer(pubsub);
         server = wsApp.app.listen(4000);
-        client = new WebSocket('ws://localhost:4000/');
     });
-  
-    it('Should handle', (done) => {
+    
+    it('Should handle block stream', (done) => {
+        client = new WebSocket('ws://localhost:4000/blocks');
+        client.on('message', async (block) => {
+            // assert
+            done(new Error("ss"));
+        });
         client.on('open', () => {
-            client.send('something');
-        });
-
-        client.on('message', async (data) => {
-            console.log(data);
-            done();
-        });
+            pubsub.on_block(123);
+        })
     });
 
     afterEach(async () => {
         client.terminate();
         server.close();
-        wsApp.getWss().close();;
+        wsApp.getWss().close();
     });
-  
 });
